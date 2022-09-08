28cb99a8ae270a2f84f0cb4448d79b09bf7782e4
Loliconera <69399372+Loliconera@users.noreply.github.com>
2022-09-08 00:22:44 -0500

    Sentencias de control

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+public class Main
+{
+    public static void main(String[] args)
+
+    {
+        {
+            int numeroIf = 1;
+            if (numeroIf > 0) {
+                System.out.println(numeroIf + " Es Positivo");
+            } else if (numeroIf < 0) {
+                System.out.println(numeroIf + " Es Negativo");
+            } else {
+                System.out.println(numeroIf + " Es Igual");
+            }
+            {
+                System.out.println("Fin de las Condicionales");
+            }
+        }
+
+        {
+            int numeroWhile = -10;
+            while (numeroWhile < 3)
+            {
+                System.out.println(numeroWhile);
+                numeroWhile = numeroWhile +1;
+            }
+            {
+                System.out.println("Fin del Bucle While");
+            }
+        }
+
+        {
+            int numeroDoWhile = 2;
+            do
+            {
+                System.out.println(numeroDoWhile);
+                numeroDoWhile = numeroDoWhile +1;
+            } while (numeroDoWhile < 3);
+            {
+                System.out.println("Fin del Bucle DoWhile");
+            }
+        }
+
+        {
+            for (int numeroFor = 0; numeroFor <= 3; numeroFor++)
+            {
+                System.out.println(numeroFor);
+            }
+            {
+                System.out.println("Fin del Bucle For");
+            }
+        }
+        {
+            var Estación = "INVIERNO";
+            switch(Estación)
+            {
+                case "PRIMAVERA":
+                    System.out.println("Es PRIMAVERA");
+                    break;
+                case "VERANO":
+                    System.out.println("Es VERANO");
+                    break;
+                case "OTOÑO":
+                    System.out.println("Es OTOÑO");
+                    break;
+                case "INVIERNO":
+                    System.out.println("Es INVIERNO");
+                    break;
+                default:
+                    System.out.println("No es una Estación");
+                    break;
+            }
+            {
+                System.out.println("Fin del Switch Case");
+            }
+        }
+
+    }
+
+}
